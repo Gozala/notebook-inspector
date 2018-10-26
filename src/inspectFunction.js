5793593d5781283cd5507f1c8bfcf6351fc6f3ee
Irakli Gozalishvili <rfobic@gmail.com>
2018-10-25 20:37:40 -0700

    Implement getters + prototype chain support

--- src/inspectFunction.js
+++ src/inspectFunction.js
@@ -1,59 +1,119 @@
 var toString = Function.prototype.toString,
-    TYPE_ASYNC = {prefix: "async ƒ"},
-    TYPE_ASYNC_GENERATOR = {prefix: "async ƒ*"},
-    TYPE_CLASS = {prefix: "class"},
-    TYPE_FUNCTION = {prefix: "ƒ"},
-    TYPE_GENERATOR = {prefix: "ƒ*"};
+  TYPE_ASYNC = { prefix: "async ƒ" },
+  TYPE_ASYNC_GENERATOR = { prefix: "async ƒ*" },
+  TYPE_CLASS = { prefix: "class" },
+  TYPE_FUNCTION = { prefix: "ƒ" },
+  TYPE_ARROW_FUNCTION = { prefix: "λ" },
+  TYPE_GENERATOR = { prefix: "ƒ*" }
 
 export default function inspectFunction(f) {
-  var type, m, t = toString.call(f);
+  var type,
+    m,
+    t = toString.call(f)
 
   switch (f.constructor && f.constructor.name) {
-    case "AsyncFunction": type = TYPE_ASYNC; break;
-    case "AsyncGeneratorFunction": type = TYPE_ASYNC_GENERATOR; break;
-    case "GeneratorFunction": type = TYPE_GENERATOR; break;
-    default: type = /^class\b/.test(t) ? TYPE_CLASS : TYPE_FUNCTION; break;
+    case "AsyncFunction":
+      type = TYPE_ASYNC
+      break
+    case "AsyncGeneratorFunction":
+      type = TYPE_ASYNC_GENERATOR
+      break
+    case "GeneratorFunction":
+      type = TYPE_GENERATOR
+      break
+    default: {
+      switch (t.substr(0, 4)) {
+        case "clas": {
+          type = TYPE_CLASS
+          break
+        }
+        case "func": {
+          type = TYPE_FUNCTION
+          break
+        }
+        case "get ": {
+          type = TYPE_FUNCTION
+          break
+        }
+        case "set ": {
+          type = TYPE_FUNCTION
+          break
+        }
+        default: {
+          type = TYPE_ARROW_FUNCTION
+          break
+        }
+      }
+      break
+    }
   }
 
   // A class, possibly named.
   // class Name
   if (type === TYPE_CLASS) {
-    return formatFunction(type, f.name || "");
+    return formatFunction(type, f.name || "")
   }
 
   // An arrow function with a single argument.
   // foo =>
   // async foo =>
-  if (m = /^(?:async\s*)?(\w+)\s*=>/.exec(t)) {
-    return formatFunction(type, "(" + m[1] + ")");
+  if ((m = /^(?:async\s*)?(\w+)\s*=>/.exec(t))) {
+    return formatFunction(type, "(" + m[1] + ")")
   }
 
   // An arrow function with parenthesized arguments.
   // (…)
   // async (…)
-  if (m = /^(?:async\s*)?\(\s*(\w+(?:\s*,\s*\w+)*)?\s*\)/.exec(t)) {
-    return formatFunction(type, m[1] ? "(" + m[1].replace(/\s*,\s*/g, ", ") + ")" : "()");
+  if ((m = /^(?:async\s*)?\(\s*(\w+(?:\s*,\s*\w+)*)?\s*\)/.exec(t))) {
+    return formatFunction(
+      type,
+      m[1] ? "(" + m[1].replace(/\s*,\s*/g, ", ") + ")" : "()"
+    )
   }
 
   // A function, possibly: async, generator, anonymous, simply arguments.
   // function name(…)
   // function* name(…)
+  // get name(…)
+  // set name(…)
   // async function name(…)
   // async function* name(…)
-  if (m = /^(?:async\s*)?function(?:\s*\*)?(?:\s*\w+)?\s*\(\s*(\w+(?:\s*,\s*\w+)*)?\s*\)/.exec(t)) {
-    return formatFunction(type, (f.name || "") + (m[1] ? "(" + m[1].replace(/\s*,\s*/g, ", ") + ")" : "()"));
+  if (
+    (m = /^(?:async\s*)?function(?:\s*\*)?(?:\s*\w+)?\s*\(\s*(\w+(?:\s*,\s*\w+)*)?\s*\)/.exec(
+      t
+    ))
+  ) {
+    return formatFunction(
+      type,
+      (f.name || "") +
+        (m[1] ? "(" + m[1].replace(/\s*,\s*/g, ", ") + ")" : "()")
+    )
   }
 
   // Something else, like destructuring, comments or default values.
-  return formatFunction(type, (f.name || "") + "(…)");
+  return formatFunction(type, displayName(f.name || "") + "(…)")
+}
+
+const displayName = name => {
+  switch (name.substr(0, 4)) {
+    case "get ": {
+      return name.substr(4)
+    }
+    case "set ": {
+      return name.substr(4)
+    }
+    default: {
+      return name
+    }
+  }
 }
 
 function formatFunction(type, name) {
-  var span = document.createElement("span");
-  span.className = "observablehq--function";
-  var spanType = span.appendChild(document.createElement("span"));
-  spanType.className = "observablehq--keyword";
-  spanType.textContent = type.prefix;
-  span.appendChild(document.createTextNode(" " + name));
-  return span;
+  var span = document.createElement("span")
+  span.className = "observablehq--function"
+  var spanType = span.appendChild(document.createElement("span"))
+  spanType.className = "observablehq--keyword"
+  spanType.textContent = type.prefix
+  span.appendChild(document.createTextNode(" " + name))
+  return span
 }
--- src/object.js
+++ src/object.js
@@ -1,26 +1,60 @@
-const {getOwnPropertySymbols, prototype: {hasOwnProperty}} = Object;
-const {toStringTag} = Symbol;
+const {
+  getOwnPropertySymbols,
+  getOwnPropertyDescriptor,
+  getOwnPropertyNames,
+  getPrototypeOf,
+  prototype: { hasOwnProperty }
+} = Object
+const { toStringTag } = Symbol
 
-export const FORBIDDEN = {};
+export const FORBIDDEN = {}
 
-export const symbolsof = getOwnPropertySymbols;
+export const symbolsof = getOwnPropertySymbols
+export const namesof = getOwnPropertyNames
+export const propertyof = getOwnPropertyDescriptor
 
 export function isown(object, key) {
-  return hasOwnProperty.call(object, key);
+  return hasOwnProperty.call(object, key)
 }
 
 export function tagof(object) {
-  return object[toStringTag]
-      || (object.constructor && object.constructor.name)
-      || "Object";
+  return (
+    object[toStringTag] ||
+    (object.constructor && object.constructor.name) ||
+    "Object"
+  )
 }
 
 export function valueof(object, key) {
   try {
-    const value = object[key];
-    if (value) value.constructor; // Test for SecurityError.
-    return value;
+    const value = object[key]
+    if (value) value.constructor // Test for SecurityError.
+    return value
   } catch (ignore) {
-    return FORBIDDEN;
+    return FORBIDDEN
+  }
+}
+
+export function* fieldsof(object) {
+  for (const key of namesof(object)) {
+    yield key
+  }
+
+  for (const symbol of symbolsof(object)) {
+    yield symbol
+  }
+}
+
+export function* gettersof(object) {
+  let proto = getPrototypeOf(object)
+  while (proto) {
+    for (const key of fieldsof(proto)) {
+      const property = propertyof(proto, key)
+      if (property.get) {
+        yield key
+      }
+    }
+
+    proto = getPrototypeOf(proto)
   }
 }
